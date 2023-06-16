17ed48250b4956ced944e9159c10543e8022398e
iamtushar007 <81921024+iamtushar007@users.noreply.github.com>
2023-06-16 22:07:09 +0530

    Add files via upload

--- DiceProblemGame.java
+++ DiceProblemGame.java
@@ -1,8 +1,36 @@
 import java.util.ArrayList;
 
 public class DiceProblemGame {
+
     static ArrayList<String> diceGame(int currentValue, int endValue) {
-        return null;
+        // Part of the result (10 Reach)
+        // Exceed 10 (Not Part of the result)
+        if (currentValue == endValue) {
+            // Part of the result (10 Reach)
+            ArrayList<String> result = new ArrayList<>();
+            result.add("");
+            return result;
+        }
+
+        if (currentValue > endValue) {
+            // Exceed 10 (Not Part of the result)
+            ArrayList<String> result = new ArrayList<>();
+            return result;
+        } else
+            return null;
+        // 1 to 6 Calls?
+
+        // Roll the Dice (Can be 1 to 6)
+
+        ArrayList<String> finalResult = new ArrayList<>();
+        for (int dice = 1; dice <= 6; dice++) {
+            int newValue = currentValue + dice;
+            ArrayList<String> list = diceGame(newValue, endValue);
+            for (String s : list) {
+                finalResult.add(dice + s);
+            }
+        }
+        return finalResult;
     }
 
     public static void main(String[] args) {
